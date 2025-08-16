b86950f648312af3a35542d566900f968b9b3540
spkumar-challa <shanmukhapadmakumar.challa@gmail.com>
2025-08-16 22:25:08 +0530

    Implemented admin role/status update API

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -5,14 +5,14 @@ const getFilteredUsers = async(req, res) =>{
 
     const filter = {};
 
-    if(role) filter.role = role;
-    if(banned) filter.banned = banned === 'true';
-    if(deleted) filter.deleted = deleted === 'true';  
+    if(role !== undefined) filter.role = role;
+    if(banned !== undefined) filter.banned = banned === 'true';
+    if(deleted !== undefined) filter.deleted = deleted === 'true';  
     
     try{     
         const users = await User.find(filter).select("-password");
 
-        res.status(200).json({message: users});
+        res.status(200).json({message: "Users fetched successfully", data: users});
 
     }catch(err){
         res.status(500).json({message: "Server Error", error: err.message});
@@ -20,13 +20,30 @@ const getFilteredUsers = async(req, res) =>{
 };
 
 const updateUserStatus = async(req, res) =>{
-    const {userId, role, banned, deleted} = req.query;
+    const userId = req.params.id;
+    const {role, banned, deleted} = req.query;
 
     const updateRoleOrStatus = {};
 
-     if(role) filter.role = role;
-    if(banned) filter.banned = banned === 'true';
-    if(deleted) filter.deleted = deleted === 'true';
+    if (role !== undefined) updateRoleOrStatus.role = role;
+    if (banned !== undefined) updateRoleOrStatus.banned = banned === 'true';
+    if (deleted !== undefined) updateRoleOrStatus.deleted = deleted === 'true';
+
+
+    try{
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { $set: updateRoleOrStatus},
+            { new: true}
+        ).select( "-password" );
+        if(!updatedUser){
+            return res.status(404).json({message: "User not found"});
+        }
+        return res.status(200).json({message: "User updated successfully", user: updatedUser});
+    }
+    catch(err){
+        res.status(500).json({message: "Server Error", error: err.message});
+    }
 };
 
-module.exports = {getFilteredUsers};
+module.exports = {getFilteredUsers, updateUserStatus};
